3ef8730ec5f4ae8de13795e47f2260399804de4e
24fka1 <80920992+24fka1@users.noreply.github.com>
2021-11-02 00:36:56 -0400

    Add files via upload

---
+++ euler6.py
@@ -0,0 +1,17 @@
+"computes the difference betweem the sum of squares and the square of the sum"
+def sum_of_squares(x):
+    sum = 0
+    for i in range(x+1):
+        sum += (i**2)
+    return sum
+
+def square_of_sum(x):
+    sum = 0
+    for i in range(x+1):
+        sum += i
+    return sum**2
+
+if __name__ == '__main__':
+    input = int(input("please enter a positive number\n"))
+    ans = square_of_sum(input) - sum_of_squares(input)
+    print("The differnce between the sum of squares and square of the sum is {}".format(ans))
